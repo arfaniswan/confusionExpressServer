3513be234178297f48ca10ad3fe1f0b0463a030b
arfaniswan <arfaniswan@gmail.com>
2019-09-13 19:39:34 +0100

    assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    
+}, {
+    timestamps: true
+});
+
+
+var Leaders = mongoose.model('leader', leaderSchema);
+
+
+module.exports = Leaders;
