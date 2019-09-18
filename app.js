109fa4c8fb91b0c5b240cc084b9f3250864fa8a8
arfaniswan <arfaniswan@gmail.com>
2019-09-18 13:05:41 +0100

    passport jwt

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -38,15 +39,8 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+
 app.use(passport.initialize());
-app.use(passport.session());
 
 
 app.use('/', indexRouter);
@@ -54,20 +48,7 @@ app.use('/users', usersRouter);
 
 
 
-function auth (req, res, next) {
-  console.log(req.user);
 
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
